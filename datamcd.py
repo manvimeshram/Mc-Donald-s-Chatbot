c028fbbc4f5f6a1866028f5be2150388900bfb34
Manvi Meshram <meshrammanvi1999@gmail.com>
2025-06-13 13:36:40 +0530

    Add files via upload

---
+++ datamcd.py
@@ -0,0 +1,47 @@
+# data.py
+
+data = {
+    "greetings": [
+        "hello", "hi", "hey", "good morning", "good afternoon", "good evening",
+        "welcome to mcdonalds", "hi there", "yo", "hey mcdonald's"
+    ],
+    "responses": [
+        "Welcome to McDonald's! 🍔", "Hi there! Craving something tasty?", "Hello! Ready to order?",
+        "Hey! What would you like from our menu?", "Good to see you at McDonald's!"
+    ],
+    "farewells": [
+        "bye", "goodbye", "see you later", "thank you", "take care", "exit", "that's all", "done ordering"
+    ],
+    "farewell_responses": [
+        "Thanks for visiting McDonald's! 🍟", "Goodbye and enjoy your meal!", "Hope to see you soon!",
+        "Take care! Come back hungry!", "Bye! Don't forget the fries!", "Order complete. Have a great day!"
+    ],
+    "questions": [
+        "what's on the menu", "what are your opening hours", "do you have vegetarian options", 
+        "what are the best sellers", "how much is a big mac", "can I customize my order", 
+        "do you deliver", "what are today's deals", "is breakfast available now"
+    ],
+    "question_responses": [
+        "You can check out our full menu including burgers, fries, desserts, and drinks!",
+        "Most McDonald's locations are open from 6 AM to 11 PM, but it may vary.",
+        "Yes, we have vegetarian and vegan options like McAloo Tikki and salads!",
+        "Our best sellers include the Big Mac, McChicken, and French Fries.",
+        "A Big Mac typically costs around ₹130–₹150 depending on the location.",
+        "Absolutely! You can add or remove ingredients while ordering.",
+        "Yes, we deliver via apps like Swiggy, Zomato, and Uber Eats!",
+        "Check our app or website for the latest deals and discounts!",
+        "Breakfast is available from 6 AM to 11 AM in most outlets."
+    ],
+    "small_talk": [
+        "tell me a fun fact", "who owns mcdonalds", "what's your favorite burger", 
+        "what's your most popular item", "what's the secret sauce", "what's up"
+    ],
+    "small_talk_responses": [
+        "Did you know? McDonald's serves over 69 million people every day!",
+        "McDonald's was founded by Richard and Maurice McDonald and expanded globally by Ray Kroc.",
+        "I'm a bot, but if I could eat, I'd go for a Big Mac with extra sauce!",
+        "French Fries are one of our most loved menu items worldwide!",
+        "The secret sauce is... well, still a secret! 😉",
+        "Not much, just flipping virtual burgers here!"
+    ],
+}
